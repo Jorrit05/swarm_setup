e81353f1204a866fab05633dca914b69b53c602b
Jorrit Stutterheim <jorrit@Jorrits-MBP.home>
2023-01-20 17:30:39 +0100

    added publish

--- gateway_service/gateway_service.go
+++ gateway_service/gateway_service.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"log"
 
 	"github.com/Jorrit05/GoLib"
@@ -25,6 +26,40 @@ func main() {
 
 	for msg := range messages {
 		log.Printf("Received message: %v", string(msg.Body))
+		if msg.Priority == 3 {
+			publish(inputQueue.Name)
+		}
+
 		msg.Headers.Validate()
 	}
+}
+
+type ExampleMessage struct {
+	Name string `json:"name"`
+	Age  int    `json:"age"`
+}
+
+func publish(queueName string) {
+	// conn, err := GoLib.Connect()
+	// if err != nil {
+	// 	log.Fatalf("Failed to connect to RabbitMQ: %v", err)
+	// }
+	// defer conn.Close()
+
+	// outputQueue, err := GoLib.DeclareQueue("output_queue")
+	// if err != nil {
+	// 	log.Fatalf("Failed to declare output queue: %v", err)
+	// }
+
+	exampleMessage := ExampleMessage{Name: "John Smith", Age: 30}
+	jsonMessage, err := json.Marshal(exampleMessage)
+	if err != nil {
+		log.Fatalf("Failed to marshal message: %v", err)
+	}
+
+	err = GoLib.Publish(queueName, jsonMessage)
+	if err != nil {
+		log.Fatalf("Failed to publish message: %v", err)
+	}
+	log.Printf("Sent message: %v", string(jsonMessage))
 }
