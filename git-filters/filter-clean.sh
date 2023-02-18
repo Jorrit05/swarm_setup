8fcb20c5b3785adbb07a7a7ee871783fcc3fb489
J Stutterheim <jorstut@hotmail.com>
2023-02-18 17:17:36 +0100

    test

--- git-filters/filter-clean.sh
+++ git-filters/filter-clean.sh
@@ -1,4 +1,2 @@
-#!/bin/bash
-
-# Remove password_hash field from JSON files
-sed -i '' '/"password_hash"/d' $1
+#!/bin/sh
+sed -i ''  's/"password_hash": *"[^"]*"/"password_hash": "{password_hash}"/g' ../definitions3.json
--- git-filters/filter-smudge.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-sed -i ''  's/"password_hash": *"[^"]*"/"password_hash": "{password_hash}"/g' ../definitions.json
