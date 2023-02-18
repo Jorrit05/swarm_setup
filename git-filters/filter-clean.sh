5569e1f29b54a3bd4a4d6a7f7d08bb9f42606944
J Stutterheim <jorstut@hotmail.com>
2023-02-18 17:18:50 +0100

    test

--- git-filters/filter-clean.sh
+++ git-filters/filter-clean.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-sed -i ''  's/"password_hash": *"[^"]*"/"password_hash": "{password_hash}"/g' ../definitions3.json
+sed -i ''  's/"password_hash": *"[^"]*"/"password_hash": "{password_hash}"/g' definitions3.json
